8ca123dd8a12f26a45c7b0905dff1209ce0d33cd
Sebastijan Bandur <info@smikro.org>
2015-02-10 17:36:34 +0100

    Create enemyTypes.py

---
+++ enemyTypes.py
@@ -0,0 +1,10 @@
+# ENEMY TYPES enemy.type == "$"
+fangrider = "fangrider"
+ogre = "ogre"
+munchkin = "munchkin"
+thrower = "thrower"
+shaman = "shaman"
+
+
+# fences and most of doors
+other = undefined == 0 == null
