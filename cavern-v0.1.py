a25ec46d8ed0bba09fd97ea18f2914d8541f5cb0
Sebastijan Bandur <info@smikro.org>
2020-03-28 23:51:23 +0100

    Update cavern-v0.1.py

--- cavern-v0.1.py
+++ cavern-v0.1.py
@@ -32,29 +32,33 @@ while True:
     THROWER = hero.findByType("thrower")
     THROWER = hero.findNearest(THROWER)
     MUNCHKIN = hero.findByType("munchkin")
-    MUNCHKIN = hero.findNearest(SHAMAN)
+    MUNCHKIN = hero.findNearest(MUNCHKIN)
+    ENEMY = hero.findNearestEnemy()
     
     
     
     if OGRE and self.distanceTo(OGRE) < 20:
-        self.cast("chain-lightning", OGRE)
-        self.attack(OGRE)
+        if hero.canCast("chain-lightning", OGRE):
+            self.cast("chain-lightning", OGRE)
+        elif OGRE.health > 0:
+            self.bash(OGRE)
+            self.attack(OGRE)
     elif THROWER and self.distanceTo(THROWER) < 40:
-        if hero.isReady("cleave":
-            self.cleave(THROWER)
-        if
-        self.attack(THROWER)
+        if hero.canCast("chain-lightning", THROWER):
+            hero.cast("chain-lightning", THROWER)
+        elif THROWER.health > 0:
+            self.attack(THROWER)
     elif BRAWLER and self.distanceTo(BRAWLER) < 20:
         self.attack(BRAWLER)
-    elif merlin and self.distanceTo(merlin) < 20:
-           self.attack(merlin)
-    elif munchkin and self.distanceTo(munchkin) < 20:
-            self.attack(munchkin)
-    elif shaman and self.distanceTo(shaman) < 20:
-        if cleave:
-            self.cleave(shaman)
-        self.attack(shaman)
-    
-    elif enemy and self.distanceTo(enemy) < 10 and enemy.health < 1:
-        self.attack(enemy)
+    elif MERLIN and self.distanceTo(MERLIN) < 20:
+           self.attack(MERLIN)
+    elif MUNCHKIN and self.distanceTo(MUNCHKIN) < 20:
+            self.attack(MUNCHKIN)
+    elif SHAMAN and self.distanceTo(SHAMAN) < 20:
+        if hero.isReady("cleave"):
+            self.cleave(SHAMAN)
+        if SHAMAN.health > 0:
+            self.attack(SHAMAN)
+    elif ENEMY and self.distanceTo(ENEMY) < 10 and ENEMY.health < 1:
+        self.attack(ENEMY)
    
