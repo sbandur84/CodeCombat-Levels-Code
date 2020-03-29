9b7a389444e383ea5d6524c3049bde7552d943a4
Sebastijan Bandur <info@smikro.org>
2020-03-29 01:57:34 +0100

    Update cavern-v0.1.py
    dober rezultat na levi

--- cavern-v0.1.py
+++ cavern-v0.1.py
@@ -16,7 +16,7 @@ def Kill(e):
 
 
 
-
+hero.moveXY(30, 100)
 
 
 while True:
@@ -43,31 +43,57 @@ while True:
     HERO = [e for e in self.findEnemies() if e.id in ["Hero Placeholder", "Hero Placeholder 1"]][0]
     
     
-    if FANGRIDER and self.distanceTo(FANGRIDER) < 50 and FANGRIDER.health > 0:
-        if hero.canCast("chain-lightning", FANGRIDER):
-            self.cast("chain-lightning", FANGRIDER)
-        elif FANGRIDER.health > 0:
-            self.attack(FANGRIDER)
+    if BURL and self.distanceTo(BURL) < 20:
+        if hero.canCast("chain-lightning", BURL):
+            self.cast("chain-lightning", BURL)
+        elif BURL.health > 0:
+            hero.shield()
+            if hero.isReady("bash"):
+                if hero.canElectrocute(BURL):
+                    hero.electrocute(BURL)
+                self.bash(BURL)
+                self.shield()
+                
+            if BURL.health > 0:
+                self.attack(BURL)
+                self.shield()
     elif OGRE and self.distanceTo(OGRE) < 20:
         if hero.canCast("chain-lightning", OGRE):
             self.cast("chain-lightning", OGRE)
         elif OGRE.health > 0:
             hero.shield()
             if hero.isReady("bash"):
+                if hero.canElectrocute(OGRE):
+                    hero.electrocute(OGRE)
                 self.bash(OGRE)
                 self.shield()
             if OGRE.health > 0:
                 self.attack(OGRE)
                 self.shield()
+    elif FANGRIDER and self.distanceTo(FANGRIDER) < 50 and FANGRIDER.health > 0:
+        if hero.canCast("chain-lightning", FANGRIDER):
+            self.cast("chain-lightning", FANGRIDER)
+        elif FANGRIDER.health > 0:
+            if hero.canElectrocute(FANGRIDER):
+                hero.electrocute(FANGRIDER)
+            self.attack(FANGRIDER)
     elif THROWER and self.distanceTo(THROWER) < 30:
         if THROWER.health > 0:
             self.attack(THROWER)
+    elif SKELETON and self.distanceTo(SKELETON) < 20:
+        if hero.canElectrocute(OGRE):
+            hero.electrocute(OGRE)
+        self.attack(SKELETON)
     elif BRAWLER and self.distanceTo(BRAWLER) < 20:
+        if hero.canElectrocute(BRAWLER):
+            hero.electrocute(BRAWLER)
         self.attack(BRAWLER)
     elif HERO and self.distanceTo(HERO) < 15:
         if hero.canCast("chain-lightning", HERO):
             self.cast("chain-lightning", HERO)
         elif HERO.health > 0:
+            if hero.canElectrocute(HERO):
+                hero.electrocute(HERO)
             self.bash(HERO)
             self.shield()
             self.attack(HERO)
